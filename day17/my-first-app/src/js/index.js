46a51b7d5f9fad9e080ee52653b407362df02a2c
vishnu11006271 <96375978+vishnu11006271@users.noreply.github.com>
2022-04-27 21:46:57 +0530

    Delete day17/my-first-app directory

--- day17/my-first-app/src/js/index.js
+++
@@ -1,7 +0,0 @@
-// Write your code here...
-var ele = document.getElementById('main');
-ele.innerHTML = "Hello javascript";
-console.log(ele);
-ele.style.color = 'red';
-ele.style.backgroundColor = 'yellow';
-ele.style.fontFamily = 'cursive';              
